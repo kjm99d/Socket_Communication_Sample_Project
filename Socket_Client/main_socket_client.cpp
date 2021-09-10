a26d017855d8e3dee24c51712e27365cfffaf60e
DESKTOP-PADH56F\Eddy <kjm99d@naver.com>
2021-09-11 01:22:28 +0900

    # Project Create - sample project upload - .gitignore 작성 (Build File Path ignore) - utf-8 setting upload (.editorconfig)

---
+++ Socket_Client/main_socket_client.cpp
@@ -0,0 +1,52 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <WinSock2.h>
+
+#pragma comment(lib, "ws2_32")
+
+
+#define PORT		4578
+#define PACKET_SIZE 1024
+#define SERVER_IP	"127.0.0.1"
+
+
+int main()
+{
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+
+	SOCKET hSocket;
+	hSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	SOCKADDR_IN tAddr = {};
+	tAddr.sin_family = AF_INET;
+	tAddr.sin_port = htons(PORT);
+	tAddr.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+	connect(hSocket, (SOCKADDR*)&tAddr, sizeof(tAddr));
+
+
+	while (1)
+	{
+		// i want no disconnect;
+		// server is packer send
+
+	//	char cMsg[] = "Client Send";
+	//	send(hSocket, cMsg, strlen(cMsg), 0);
+
+		char cBuffer[PACKET_SIZE] = {};
+		recv(hSocket, cBuffer, PACKET_SIZE, 0);
+
+		printf("Recv Msg : %s \n", cBuffer);
+	}
+	closesocket(hSocket);
+
+	WSACleanup();
+
+	getchar();
+
+	return 0;
+}
---
+++ Socket_Server/main_socket_server.cpp
@@ -0,0 +1,61 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <stdio.h>
+#include <WinSock2.h>
+#include <Windows.h>
+
+#pragma comment(lib, "ws2_32")
+
+#define PORT		4578
+#define PACKET_SIZE 1024
+
+
+int main()
+{
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+
+	SOCKET hListen;
+	hListen = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	SOCKADDR_IN tListenAddr = {};
+	tListenAddr.sin_family = AF_INET;
+	tListenAddr.sin_port = htons(PORT);
+	tListenAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	bind(hListen, (SOCKADDR*)&tListenAddr, sizeof(tListenAddr));
+	listen(hListen, SOMAXCONN);
+
+	SOCKADDR_IN tClntAddr = {};
+	int iClntSize = sizeof(tClntAddr);
+	SOCKET hClient = accept(hListen, (SOCKADDR*)&tClntAddr, &iClntSize);
+
+	while (1)
+	{
+
+		//char cBuffer[PACKET_SIZE] = {};
+		//recv(hClient, cBuffer, PACKET_SIZE, 0);
+		//printf("Recv Msg : %s \n", cBuffer);
+
+
+
+		char cMsg[] = "Server Send";
+		send(hClient, cMsg, strlen(cMsg), 0);
+
+		Sleep(1000);
+	}
+
+
+	closesocket(hClient);
+	closesocket(hListen);
+
+
+	WSACleanup();
+
+	getchar();
+
+	return 0;
+}
